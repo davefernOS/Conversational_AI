52d9f86b87fe07bf99e93580e1f780eeedf9bbb6
davefernOS <davefern@umich.edu>
2020-04-02 12:55:25 -0300

    Trying to just get a website to run

--- js/index.jsx
+++ js/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Speech from './speech';
 
 // This method is only called once
 ReactDOM.render(
   // Insert the component into the DOM
+  <Speech url="/speech/" />,
   document.getElementById('reactEntry'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,19 @@ module.exports = {
     path: path.join(__dirname, '/static/'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+    {
+      test: /\.jsx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/react']
+        }
+      }
+    }
+  ]
+  },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
